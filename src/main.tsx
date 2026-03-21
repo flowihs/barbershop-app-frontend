e7680b962582a4e712b7273ab7e0aea4b99c462d
mikadzee <asansabatyrov@gmail.com>
2026-03-21 13:27:19 +0300

    feat: добавление файлов стилей

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './app/styles/global.css';
+import './app/styles/global.less';
 import App from './app/App';
 
 
