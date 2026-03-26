4519a20b76a8f35c303d438576cdd78e2e7051c2
mikadzee <asansabatyrov@gmail.com>
2026-03-26 19:04:49 +0300

    fix: удален reset.less, tailwind.less, вернул global.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './app/styles/global.less';
+import './app/styles/global.css';
 import App from './app/App';
 
 
