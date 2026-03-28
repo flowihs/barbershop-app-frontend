282c21bfaaa19ab4fa84dd38e78143e0a070d0a9
mikadzee <asansabatyrov@gmail.com>
2026-03-28 09:34:56 +0300

    feat: добавил контент на страницу профиля

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -1,20 +1,38 @@
 import { useNavigate } from 'react-router';
+import { useQuery } from '@tanstack/react-query';
+import { useLaunchParams } from '@telegram-apps/sdk-react';
+import { accountService } from '../../entities/account/api/accountApi';
+import Header from '../../widgets/profile/Header';
+import ActionsButton from '../../widgets/profile/ActionsButtons';
+import './profile.less';
 
 function ProfilePage() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const tgWebAppData = useLaunchParams();
+
+  const { data: userData, isLoading, error } = useQuery({
+    queryKey: ['account', 'me'],
+    queryFn: accountService.getMe,
+  });
+  // свою дату закинь если не так чтото
+  const avatar = tgWebAppData.initData?.user?.photoUrl;
+  const name = userData?.firstName || tgWebAppData.initData?.user?.firstName || 'no name';
+  const description = userData?.description || 'no description';
+  const rating = userData?.rating || 4.5;
+
 
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Profile</h1>
-      <p className="mb-4">Your appointments</p>
-      <button
-        className="text-blue-500 underline"
-        onClick={() => navigate('/')}
-      >
-        Home
-      </button>
-    </div>
-  )
+		<div className='profile-page'>
+			<Header
+				avatar={avatar}
+				name={name}
+				description={description}
+				rating={rating}
+			/>
+
+			<ActionsButton onSchedule={() => navigate('/schedule')} onEdit={() => navigate('/settings')} />
+		</div>
+	)
 }
 
-export default ProfilePage
+export default ProfilePage;
